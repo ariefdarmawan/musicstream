fcc298c3d0ba47383cc0823270eae3eb33e8244e
Arief Darmawan <ariefda@hotmail.com>
2016-05-17 06:00:58 +0700

    add / handler

--- web/start/main.go
+++ web/start/main.go
@@ -3,9 +3,14 @@ package main
 import (
     "github.com/eaciit/knot/knot.v1"
     "eaciit/musicstream/web"
+    "net/http"
 )
 
 func main() {
     app := webapp.App()
-    knot.StartApp(app, "localhost:8027")
+    knot.StartAppWithFn(app, "localhost:8027",
+        map[string]knot.FnContent{"/":func(ctx *knot.WebContext)interface{}{
+            http.Redirect(ctx.Writer, ctx.Request, "/music/index", 301)
+            return nil
+        }})
 }
